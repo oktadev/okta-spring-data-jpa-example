1aba425782d156dcf4cf99773ae2e61af79edcbf
Matt Raible <matt.raible@okta.com>
2021-01-26 16:15:54 -0700

    Upgrade to Spring Boot 2.4.2 (#4)

--- src/main/java/com/example/sample/DinosaurRepository.java
+++ src/main/java/com/example/sample/DinosaurRepository.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 public interface DinosaurRepository extends CrudRepository<Dinosaur, Long> {
     @Configuration
     static class RepositoryConfig implements RepositoryRestConfigurer {
         @Override
-        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry corsRegistry) {
             config.exposeIdsFor(Dinosaur.class);
         }
     }
